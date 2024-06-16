d866bed8070806b25f5bd3a99ea44e63d20ed47e
nupurhada <hadanupur@gmail.com>
2024-06-16 15:34:32 +0530

    Fixed database issues and adjusted redirections

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -1,33 +1,35 @@
-import { User } from '@prisma/client'
-import { AvatarProps } from '@radix-ui/react-avatar'
+import { AvatarProps } from "@radix-ui/react-avatar";
+import { Icons } from "../components/icons";
+import { Avatar, AvatarFallback } from "../components/ui/avatar";
+import Image from "next/image";
 
-import { Icons } from '../components/icons'
-import { Avatar, AvatarFallback } from '../components/ui/avatar'
-import Image from 'next/image'
-
+// Adjust props interface to remove any specific references to Prisma's User type
 interface UserAvatarProps extends AvatarProps {
-  user: Pick<User, 'image' | 'name'>
+  user: {
+    image?: string;
+    name?: string;
+  };
 }
 
 export function UserAvatar({ user, ...props }: UserAvatarProps) {
   return (
     <Avatar {...props}>
       {user.image ? (
-        <div className='relative aspect-square h-full w-full'>
+        <div className="relative aspect-square h-full w-full">
           <Image
             fill
             src={user.image}
-            alt='profile picture'
-            referrerPolicy='no-referrer'
+            alt="profile picture"
+            referrerPolicy="no-referrer"
           />
         </div>
       ) : (
         <AvatarFallback>
-          <span className='sr-only'>{user?.name}</span>
+          <span className="sr-only">{user?.name}</span>
           {/* @ts-ignore */}
-          <Icons.user className='h-4 w-4' />
+          <Icons.user className="h-4 w-4" />
         </AvatarFallback>
       )}
     </Avatar>
-  )
+  );
 }
--- components/social-proof.tsx
+++ components/social-proof.tsx
@@ -1,23 +1,23 @@
-const companies = [
-    "Google",
-    "Microsoft",
-    "Amazon",
-    "Netflix",
-    "YouTube",
-    "Instagram",
-    "Uber",
-    "Spotify",
-  ];
-  
-  export function Companies() {
-    return (
-      <section id="companies">
-        <div className="py-1 pb-10">
-          <div className="container mx-auto px-4 md:px-8">
-            <h3 className="text-center text-sm font-semibold text-gray-500 pb-2">
-              TRUSTED BY LEADING TEAMS
-            </h3>
-            <div className="relative mt-6">
+// const companies = [
+//     "Google",
+//     "Microsoft",
+//     "Amazon",
+//     "Netflix",
+//     "YouTube",
+//     "Instagram",
+//     "Uber",
+//     "Spotify",
+//   ];
+
+export function Companies() {
+  return (
+    <section id="companies">
+      <div className="py-1 pb-10">
+        <div className="container mx-auto px-4 md:px-8">
+          <h3 className="text-center text-sm font-semibold text-gray-500 pb-2">
+            TRUSTED BY LEADING TEAMS
+          </h3>
+          {/* <div className="relative mt-6">
               <div className="grid grid-cols-2 place-items-center gap-2 md:grid-cols-4 xl:grid-cols-8 xl:gap-4">
                 {companies.map((logo, idx) => (
                   <img
@@ -27,13 +27,12 @@ const companies = [
                     alt={logo}
                   />
                 ))}
-              </div>
-              <div className="pointer-events-none absolute inset-y-0 left-0 h-full w-1/3 bg-gradient-to-r from-white dark:from-black"></div>
+              </div> */}
+          {/* <div className="pointer-events-none absolute inset-y-0 left-0 h-full w-1/3 bg-gradient-to-r from-white dark:from-black"></div>
               <div className="pointer-events-none absolute inset-y-0 right-0 h-full w-1/3 bg-gradient-to-l from-white dark:from-black"></div>
-            </div>
-          </div>
+            </div> */}
         </div>
-      </section>
-    );
-  }
-  
+      </div>
+    </section>
+  );
+}
